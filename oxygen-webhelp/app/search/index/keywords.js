9eeba2d99306dee3ad0ea46cffb4177ec9d7c37c
oyoyo <feng75@189.cn>
2021-12-06 03:01:08 -0700

    Add files via upload

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,20 @@
+define(function() {var keywords=[{w:"Open",p:["p0"]},{w:"Toolkit",p:["p0"]},{w:"Clogs",p:["p1","p2","p5","p10"]},{w:"Fashion",p:["p2"]},{w:"Feedback",p:["p3"]},{w:"Glossary",p:["p4"]},{w:"Museums",p:["p5"]},{w:"on",p:["p5"]},{w:"Programming",p:["p6"]},{w:"domain",p:["p6"]},{w:"examples",p:["p6"]},{w:"Reference",p:["p7"]},{w:"Example",p:["p7","p9"]},{w:"Some",p:["p8"]},{w:"assumptions",p:["p8"]},{w:"Task",p:["p9"]},{w:"/",p:["p9"]},{w:"Steps",p:["p9"]},{w:"Traditional",p:["p10"]},{w:"in",p:["p10"]},{w:"Europe",p:["p10"]},{w:"Types",p:["p11"]},{w:"Why",p:["p12"]},{w:"this",p:["p12"]},{w:"edition",p:["p12"]},{w:"of",p:["p12"]},{w:"FontoXML?",p:["p12"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2];
+ph["p2"]=[3, 2];
+ph["p3"]=[4];
+ph["p4"]=[5];
+ph["p5"]=[6, 7, 2];
+ph["p6"]=[8, 9, 10];
+ph["p7"]=[11, 12];
+ph["p8"]=[13, 14];
+ph["p9"]=[15, 16, 17, 12];
+ph["p10"]=[18, 2, 19, 20];
+ph["p12"]=[22, 23, 24, 25, 26];
+ph["p11"]=[21];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {7:1,12:1,8:-1,10:-1,1:-1,2:1,9:-1,11:-1,13:1,3:-1,4:-1,14:-1};
+});
